0005af3cc8ee0d5bae25d9719c5f482a568d61a8
leonahpd <lionaraj97@gmail.com>
2026-03-30 14:14:52 +0000

    Initial commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://backend:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://192.168.0.104:30007/api';
 
 const api = axios.create({
   baseURL: API_URL,
